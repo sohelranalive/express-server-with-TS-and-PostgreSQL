58f68621c5bcc4127cc668a42b6fbc9ad708732c
sohelranalive <sohel.cse37@gmail.com>
2025-12-02 01:38:16 +0100

    used env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
 // const express = require("express");
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 const app = express();
 const port = 5000;
 
@@ -10,22 +13,38 @@ app.use(express.urlencoded());
 
 // Database
 const pool = new Pool({
-  connectionString: process.env.postgreSQL,
+  connectionString: `${process.env.postgreSQL}`,
 });
 
 const initDB = async () => {
   await pool.query(`
-        CREATE TABLE IF NOT EXISTS users
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(20)  NOT NULL,
-        email VARCHAR(20) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        `);
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20) NOT NULL,
+      email VARCHAR(20) UNIQUE NOT NULL,
+      age INT,
+      phone VARCHAR(15),
+      address TEXT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    );
+  `);
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS toods(
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    tittle VARCHAR(200) NOT NULL,
+    description TEXT,
+    completed BOOLEAN DEFAULT false,
+    due_date DATE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `);
 };
 
-// initDB();
+initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
